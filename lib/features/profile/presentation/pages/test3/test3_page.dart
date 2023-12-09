72c61eda760c1877977a2f46d49ea308508ad783
https://github.com/Cholponai01 <cholponai.amangeldikyzy.bj@gmail.com>
2023-12-09 23:56:45 +0600

    new pages

---
+++ lib/features/profile/presentation/pages/test3/test3_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Test3Page extends StatelessWidget {
+  const Test3Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
