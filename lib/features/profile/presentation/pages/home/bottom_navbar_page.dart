14ea38c318a4572be94a78de9a2fac87f2f1fd7a
zarlyk.zhanybekov.bj@gmail.com <zarlyk.zhanybekov.bj@gmail.com>
2023-12-11 10:23:28 +0700

    Completed the bottom navbar

--- lib/features/profile/presentation/pages/home/bottom_navbar_page.dart
+++ lib/features/profile/presentation/pages/home/bottom_navbar_page.dart
@@ -26,44 +26,93 @@ class _BottomNavBarPageState extends State<BottomNavBarPage> {
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: const Color(0xffCCD1F3),
         backgroundColor: const Color(0xff2D7AE5),
         items: <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            label: 'Store',
-            icon: SvgPicture.asset(
-              "assets/bottom_icons/shopping_cart.svg",
-              // ignore: deprecated_member_use
-              color:
-                  _currentIndex == 0 ? Colors.white : const Color(0xffCCD1F3),
-              width: 25,
-              height: 25,
-            ),
-          ),
+              label: 'Store',
+              icon: _currentIndex == 0
+                  ? Container(
+                      height: 56,
+                      width: 56,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(28),
+                          border: Border.all(color: Colors.blue),
+                          color: const Color(0xff1EC2FF)),
+                      child: SvgPicture.asset(
+                        "assets/bottom_icons/shopping_cart.svg",
+                        // ignore: deprecated_member_use
+                        color: _currentIndex == 0
+                            ? Colors.white
+                            : const Color(0xffCCD1F3),
+                        fit: BoxFit.none,
+                        width: 24,
+                      ),
+                    )
+                  : SvgPicture.asset(
+                      "assets/bottom_icons/shopping_cart.svg",
+                      // ignore: deprecated_member_use
+                      color: const Color(0xffCCD1F3),
+                      width: 25,
+                      height: 25,
+                    )),
           BottomNavigationBarItem(
-            label: 'Home',
-            icon: SvgPicture.asset(
-              "assets/bottom_icons/question.svg",
-              // ignore: deprecated_member_use
-              color:
-                  _currentIndex == 1 ? Colors.white : const Color(0xffCCD1F3),
-              width: 25,
-              height: 25,
-            ),
-          ),
+              label: 'Home',
+              icon: _currentIndex == 1
+                  ? Container(
+                      height: 56,
+                      width: 56,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(28),
+                          border: Border.all(color: Colors.blue),
+                          color: const Color(0xff1EC2FF)),
+                      child: SvgPicture.asset(
+                        "assets/bottom_icons/question.svg",
+                        // ignore: deprecated_member_use
+                        color: _currentIndex == 1
+                            ? Colors.white
+                            : const Color(0xffCCD1F3),
+                        fit: BoxFit.none,
+                        width: 24,
+                      ),
+                    )
+                  : SvgPicture.asset(
+                      "assets/bottom_icons/question.svg",
+                      // ignore: deprecated_member_use
+                      color: const Color(0xffCCD1F3),
+                      width: 25,
+                      height: 25,
+                    )),
           BottomNavigationBarItem(
-            label: 'Settings',
-            icon: SvgPicture.asset(
-              "assets/bottom_icons/settings.svg",
-              // ignore: deprecated_member_use
-              color:
-                  _currentIndex == 2 ? Colors.white : const Color(0xffCCD1F3),
-              width: 25,
-              height: 25,
-            ),
-          ),
+              label: 'Settings',
+              icon: _currentIndex == 2
+                  ? Container(
+                      height: 56,
+                      width: 56,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(28),
+                          border: Border.all(color: Colors.blue),
+                          color: const Color(0xff1EC2FF)),
+                      child: SvgPicture.asset(
+                        "assets/bottom_icons/settings.svg",
+                        // ignore: deprecated_member_use
+                        color: _currentIndex == 2
+                            ? Colors.white
+                            : const Color(0xffCCD1F3),
+                        fit: BoxFit.none,
+                        width: 24,
+                      ),
+                    )
+                  : SvgPicture.asset(
+                      "assets/bottom_icons/settings.svg",
+                      // ignore: deprecated_member_use
+                      color: const Color(0xffCCD1F3),
+                      width: 25,
+                      height: 25,
+                    )),
         ],
         currentIndex: _currentIndex,
-        selectedItemColor: const Color(0xff236681),
+        selectedItemColor: const Color(0xffCCD1F3),
         onTap: _onTabTapped,
       ),
     );
