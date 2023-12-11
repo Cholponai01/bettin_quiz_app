2e94bdb4b08d29c54abf3b6b6e3b868499897055
zarlyk.zhanybekov.bj@gmail.com <zarlyk.zhanybekov.bj@gmail.com>
2023-12-11 14:57:41 +0700

    The home page is half done

--- lib/features/profile/presentation/pages/home/home_page.dart
+++ lib/features/profile/presentation/pages/home/home_page.dart
@@ -1,10 +1,136 @@
 import 'package:flutter/material.dart';
+import 'package:svg_flutter/svg.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int index = 0;
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: const Color(0xFF2A4FC8),
+      appBar: AppBar(
+        backgroundColor: const Color(0xff2D7AE5),
+        title: const Text(
+          'Home',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        centerTitle: true,
+        actions: [
+          SizedBox(
+            width: 87,
+            height: 40,
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                color: const Color(0xFF2A4FC8),
+                borderRadius: BorderRadius.circular(40),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  const SizedBox(
+                    width: 37,
+                    child: Text(
+                      '50',
+                      style: TextStyle(
+                        color: Color(0xFFF8F804),
+                        fontSize: 18,
+                        fontWeight: FontWeight.w700,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Container(
+                    width: 32,
+                    height: 32,
+                    decoration: BoxDecoration(
+                      gradient: const LinearGradient(
+                        colors: [Color(0xFFF8F804), Color(0xFFFBA802)],
+                      ),
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                    child: const Icon(
+                      Icons.star,
+                      color: Color(0xffE97C01),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 16,
+          )
+        ],
+      ),
+      body: Expanded(
+        child: GridView.builder(
+          padding: const EdgeInsets.all(10.00),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, crossAxisSpacing: 5, mainAxisSpacing: 5),
+          itemCount: 8,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              padding: const EdgeInsets.all(8.0),
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage(
+                        'assets/png_images/first_image.png',
+                      ),
+                      fit: BoxFit.cover)),
+              child: Stack(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(16.0, 16, 16, 8),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '0${index + 1}',
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            for (int i = 0; i < 3; i++)
+                              SvgPicture.asset(
+                                'assets/svg_images/Star.svg',
+                                // ignore: deprecated_member_use
+                                color: const Color(0xFF2A4FC8),
+                              ),
+                          ],
+                        ),
+                        const Text(
+                          'Elementary \nlevel',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
