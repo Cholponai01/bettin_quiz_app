04523f131c43ba9f27836dae4cf65fa48d52b922
zarlyk.zhanybekov.bj@gmail.com <zarlyk.zhanybekov.bj@gmail.com>
2023-12-12 12:13:44 +0700

    The interface is compaleted

--- lib/features/profile/presentation/pages/home/home_page.dart
+++ lib/features/profile/presentation/pages/home/home_page.dart
@@ -9,6 +9,27 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final List<String> _images = [
+    'assets/png_images/first_image.png',
+    'assets/png_images/football_stadium.png',
+    'assets/png_images/soccer_teams.png',
+    'assets/png_images/soccer_terms.png',
+    'assets/png_images/soccer_player.png',
+    'assets/png_images/soccer_match.png',
+    'assets/png_images/national_team.png',
+    'assets/png_images/match_results.png',
+  ];
+
+  List<String> texts = [
+    'Elementary level',
+    'Soccer stadiums',
+    'Soccer teams',
+    'Soccer terms',
+    'Guess the soccer player',
+    'Guess a soccer match',
+    'Guess the national team',
+    'Guess match results',
+  ];
   int index = 0;
 
   @override
@@ -78,14 +99,13 @@ class _HomePageState extends State<HomePage> {
           padding: const EdgeInsets.all(16.00),
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 2, crossAxisSpacing: 5, mainAxisSpacing: 5),
-          itemCount: 8,
+          itemCount: texts.length,
           itemBuilder: (BuildContext context, int index) {
+            String pngPath = _images[index];
             return Container(
-              decoration: const BoxDecoration(
+              decoration: BoxDecoration(
                 image: DecorationImage(
-                  image: AssetImage(
-                    'assets/png_images/first_image.png',
-                  ),
+                  image: AssetImage(pngPath),
                   fit: BoxFit.cover,
                 ),
               ),
@@ -110,9 +130,9 @@ class _HomePageState extends State<HomePage> {
                             fontWeight: FontWeight.w600,
                           ),
                         ),
-                        const Text(
-                          'Elementary\nlevel',
-                          style: TextStyle(
+                        Text(
+                          texts[index],
+                          style: const TextStyle(
                             color: Colors.white,
                             fontSize: 20,
                             fontWeight: FontWeight.w700,
@@ -127,13 +147,12 @@ class _HomePageState extends State<HomePage> {
                       children: [
                         const SizedBox(width: 16),
                         for (int i = 0; i < 3; i++)
-                          SvgPicture.asset(
-                            'assets/svg_images/Star.svg',
-                            height: 37,
-                            width: 37,
-                            // ignore: deprecated_member_use
-                            color: const Color(0xFF406FD6),
-                          ),
+                          SvgPicture.asset('assets/svg_images/Star.svg',
+                              height: 37,
+                              width: 37,
+                              // ignore: deprecated_member_use
+                              color: Colors.yellow // Color(0xFF406FD6),
+                              ),
                         const SizedBox(width: 16),
                       ],
                     ),
